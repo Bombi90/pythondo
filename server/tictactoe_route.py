14616660eb30faad6a523b80fad24556af5a3e86
Bombi90 <dani.uz@hotmail.it>
2020-05-23 12:09:45 +0100

    begin

--- server/tictactoe_route.py
+++ server/tictactoe_route.py
@@ -1,5 +1,6 @@
 from app import application
 from flask import request
+import random
 
 description = "This page is to simulate a Tictactoe game"
 label = "TicTacToe"
@@ -14,8 +15,10 @@ def get_script(board_state):
                 const player_one_button = document.getElementById("player-one-button");
                 const player_two_button =document.getElementById("player-two-button");
                 const restart_button = document.getElementById("restart-button");
+                const begin_button = document.getElementById("pick-random-player");
+
                 const board_state = {board_state};
-                console.log("hola");
+
                 const post = async (turn, board, position) => {{ 
                    const response = await fetch("/tictactoe", {{
                         headers: {{ "Content-Type": "application/json" }},
@@ -32,13 +35,22 @@ def get_script(board_state):
                    const data = await response.json()
                    document.getElementById("pythondo-app-container").innerHTML = data.html
                  }}
-                 player_one_button.addEventListener("click", async event => {{
-                     const value = player_one_input.value;
-                    await post("Player 1", board_state, Number(value))
-                 }})
-                 player_two_button.addEventListener("click", async event => {{
-                     const value = player_two_input.value;
-                    await post("Player 2", board_state, Number(value))
+
+                 if(player_one_button) {{
+                    player_one_button.addEventListener("click", async event => {{
+                        const value = player_one_input.value;
+                        await post("Player 1", board_state, Number(value))
+                    }})
+                 }}
+                 if(player_two_button) {{
+                    player_two_button.addEventListener("click", async event => {{
+                        const value = player_two_input.value;
+                        await post("Player 2", board_state, Number(value))
+                    }})
+                 }}
+
+                 begin_button.addEventListener("click", async event => {{
+                    await post(false, board_state, 0)
                  }})
                 restart_button.addEventListener("click", async event => {{
                     await get()
@@ -51,15 +63,16 @@ def get_script(board_state):
 
 def get_style():
         return """<style>*{ margin:0;padding:0;box-sizing:border-box;font-family:Arial,Helvetica,sans-serif }.container{min-height:100vh;display:flex;flex-direction:column;align-items:center;justify-content:center;background:#eee}h1{font-size:5rem;margin-bottom:.5em}h2{margin-top:1em;font-size:2rem;margin-bottom:.5em}.play-area{display:grid;width:300px;height:300px;grid-template-columns:auto auto auto}.block{display:flex;width:100px;height:100px;align-items:center;justify-content:center;font-size:3rem;font-weight:700;border:3px solid #000;transition:background .2s ease-in-out}.block:hover{cursor:pointer;background:#0ff30f}.occupied:hover{background:#ff3a3a}.win{background:#0ff30f}.win:hover{background:#0ff30f}#block_0,#block_1,#block_2{border-top:none}#block_0,#block_3,#block_6{border-left:none}#block_6,#block_7,#block_8{border-bottom:none}#block_2,#block_5,#block_8{border-right:none}button{outline:0;border:4px solid green;padding:10px 20px;font-size:1rem;font-weight:700;background:0 0;transition:all .2s ease-in-out}button:hover{cursor:pointer;background:green;color:#fff}.playerWin{color:green}.computerWin{color:red}.draw{color:#ff4500}@media only screen and (max-width:600px){h1{font-size:3rem;margin-bottom:.5em}h2{margin-top:1em;font-size:1.3rem}}</style>"""
-def display_board(board, message):
+def display_board(board, message, turn):
     return f"""
             {get_style()}
             <div id="tictactoe" class="container">
                 <h1>Tic-Tac-Toe</h1>
                 <h3>{message}</h3>
                 <div>
-                    <p>Player 1 is <strong>X</strong> <input id="player-one-input" /> <button id="player-one-button">Submit</button></p>
-                    <p>Player 2 is <strong>O</strong> <input id="player-two-input" /> <button id="player-two-button">Submit</button></p>
+                    { '<button id="pick-random-player">Begin</button></p>' if turn == "Start" else ''}
+                    {'<p>Player 1 is <strong>X</strong> <input id="player-one-input" /> <button id="player-one-button">Submit</button></p>' if turn == 'Player 1' else ''}
+                    {'<p>Player 2 is <strong>O</strong> <input id="player-two-input" /> <button id="player-two-button">Submit</button></p>' if turn == 'Player 2' else ''} 
                     <p><button id="restart-button" >Restart</button></p>
                 </div>
                 <div class="play-area">
@@ -101,12 +114,19 @@ def full_board_check(board):
             return False
     # the board is full
     return True
-
+def choose_first():
+    
+    flip = random.randint(0,1)
+    
+    if flip == 0:
+        return 'Player 1'
+    else:
+        return 'Player 2'
 @application.route("/tictactoe", methods=["POST", "GET"])
 
 def tictactoe():
     if request.method == 'GET':
-        board = display_board([' ']*10, " ")
+        board = display_board([' ']*10, " ", "Start")
         return {
             "html": board
         }
@@ -117,7 +137,7 @@ def tictactoe():
 
         player1_marker, player2_marker = ("X", "O")
         who_won = ""
-        turn = content['turn']
+        turn = content['turn'] if content['turn'] else choose_first()
              
         if turn == 'Player 1':
             
@@ -151,7 +171,7 @@ def tictactoe():
                 else:
                     turn = 'Player 1'
     
-    board = display_board(the_board, who_won)
+    board = display_board(the_board, who_won, turn)
     return {
             "html": board
     }
